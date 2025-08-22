15ab845b58a39997a5a5c730c00990479f2b9b6f
Nicolas-V-BDS <nicolas@bluedaysolutions.com>
2025-08-22 19:40:11 +0000

    chore: Restore core config files to resolve build failure

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-// Register Firebase service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registration successful with scope: ', registration.scope);
-    })
-    .catch((err) => {
-      console.log('Service Worker registration failed: ', err);
-    });
-}
-
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
